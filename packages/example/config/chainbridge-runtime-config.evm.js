1a3f8530b9b4c86cbfa31ba2063767f9a4fe0b8f
devspan <7haolin@gmail.com>
2024-07-16 17:26:17 +0300

    trial rupx tn

--- packages/example/config/chainbridge-runtime-config.evm.js
+++ packages/example/config/chainbridge-runtime-config.evm.js
@@ -4,43 +4,43 @@ window.__RUNTIME_CONFIG__ = {
     chains: [
       {
         domainId: 0,
-        networkId: 4,
-        name: "Ethereum - Rinkeby",
+        networkId: 799,  // Rupaya testnet chain ID
+        name: "Rupaya Testnet",
         decimals: 18,
-        bridgeAddress: "0x691DCf269FDb2B9e4229d27fEd2FA7cEeBF660C8",
-        erc20HandlerAddress: "0x4051C29d0545923554E3aBae5a31b5fb27e5949f",
-        rpcUrl: "http://localhost:4000",
+        bridgeAddress: "0xb19D8fB0c5519280Fe9F1A8F1C521eb663D272b0",  // Replace with actual bridge address
+        erc20HandlerAddress: "0xB3af738AB8524cb8828c8b738ACA7f6969118417",  // Replace with actual ERC20 handler address
+        rpcUrl: "https://rpc.testnet.rupaya.io",
+        startBlock: "1751700",
         type: "Ethereum",
-        nativeTokenSymbol: "ETH",
+        nativeTokenSymbol: "RUPX",
         tokens: [
           {
-            address: "0x6829D0dA9f03338703E3d9fcAD6DD308202faf46",
-            name: "an ERC20",
-            symbol: "ERC20",
-            imageUri: "WETHIcon",
-            resourceId:
-              "0x000000000000000000000000000000e389d61c11e5fe32ec1735b3cd38c69500",
+            address: "0x3c0FCb54714037e3Ba2cd125c2A3127071A936E2",  // CLD Token Address on Rupaya Testnet
+            name: "CLOUD",
+            symbol: "CLD",
+            imageUri: "CLDIcon",  // Replace with actual image URI if needed
+            resourceId: "0x0000000000000000000000003c0fcb54714037e3ba2cd125c2a3127071a936e2",  // Replace with actual resource ID
           },
         ],
       },
       {
         domainId: 1,
-        networkId: 5,
-        name: "Ethereum - Goerli",
+        networkId: 97,  // Binance testnet chain ID
+        name: "Binance Testnet",
         decimals: 18,
-        bridgeAddress: "0xF014f5c6B88C7D74c846eEEEE29c55d6BB51D9B0",
-        erc20HandlerAddress: "0xa52624ac54165113913D37184AA5957DB99CF2C9",
-        rpcUrl: "http://localhost:4001",
+        bridgeAddress: "0x85E7a2b99419e3E12a38D78205d8C42cec8a994c",  // Replace with actual bridge address
+        erc20HandlerAddress: "0x235ee4e4EBfc4d97BA5E19F943157a0518C3F40A",  // Replace with actual ERC20 handler address
+        rpcUrl: "https://bsc-testnet-rpc.publicnode.com",
+        startBlock: 42139685,
         type: "Ethereum",
-        nativeTokenSymbol: "ETH",
+        nativeTokenSymbol: "BNB",
         tokens: [
           {
-            address: "0x3e9a09aBB17509951Ea6F6E221700660017683cb",
-            name: "an ERC20",
-            symbol: "ERC20",
-            imageUri: "WETHIcon",
-            resourceId:
-              "0x000000000000000000000000000000e389d61c11e5fe32ec1735b3cd38c69500",
+            address: "0xb19D8fB0c5519280Fe9F1A8F1C521eb663D272b0",  // Token Address on Binance Testnet
+            name: "Bridged Cloud",
+            symbol: "BCLD",
+            imageUri: "BWRUPXIcon",  // Replace with actual image URI if needed
+            resourceId: "0x0000000000000000000000003c0fcb54714037e3ba2cd125c2a3127071a936e2",  // Replace with actual resource ID
           },
         ],
       },
